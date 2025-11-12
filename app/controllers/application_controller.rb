1449d050db1787d1b566b89f8a409415406c945c
Beatriz <biafigmonteiro@gmail.com>
2025-11-12 19:06:06 -0300

    (feature) Add Customer Scaffold (#4)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
-  if Rails.env.development?
-    stale_when_importmap_changes
-  end
 end
